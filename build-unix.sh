0594cd2f37d13ce8d5796cd6c2ff2cab75fcad1e
Daniel Bonrath <bonne@clockwork-origins.de>
2018-05-11 14:12:01 +0200

    updated travis stuff to print versions for g++, clang and Qt

--- build-unix.sh
+++ build-unix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+g++ --version
+clang --version
+qmake --version
+
 mkdir build
 cd build
 cmake -DWITH_CLIENT=ON -DWITH_SERVER=ON -DWITH_TOOLS=ON -DWITH_CONSOLE=OFF -DWITH_TRANSLATOR=ON -DTEST_CONFIG=ON -DCMAKE_BUILD_TYPE=Release ..
