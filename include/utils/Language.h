324a206a589542eb6d80a232efa5d29fc491f799
Daniel Bonrath <bonne@clockwork-origins.de>
2020-06-26 18:28:48 +0200

    SP-974 added language handler for convenience conversions between enum and string and the enum itself

---
+++ include/utils/Language.h
@@ -0,0 +1,42 @@
+/*
+	This file is part of Spine.
+
+    Spine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Spine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Spine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Copyright 2020 Clockwork Origins
+
+#pragma once
+
+#include <QString>
+
+namespace spine {
+namespace utils {
+
+	enum Language {
+		None,
+		
+		German = 1 << 0,
+		English = 1 << 1,
+		Polish = 1 << 2,
+		Russian = 1 << 3
+	};
+	
+	class LanguageConverter {
+	public:
+		static QString convert(Language language);
+		static Language convert(const QString & language);
+	};
+
+} /* namespace utils */
+} /* namespace spine */
---
+++ src/utils/Language.cpp
@@ -0,0 +1,46 @@
+/*
+	This file is part of Spine.
+
+    Spine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Spine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Spine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Copyright 2020 Clockwork Origins
+
+#include "Language.h"
+
+#include <QMap>
+
+using namespace spine::utils;
+
+QString LanguageConverter::convert(Language language) {
+	static QMap<Language, QString> languageStrings = {
+		{ Language::German, QStringLiteral("Deutsch") },
+		{ Language::English, QStringLiteral("English") },
+		{ Language::Polish, QStringLiteral("Polish") },
+		{ Language::Russian, QStringLiteral("Russian") },
+	};
+
+	return languageStrings.value(language);
+}
+
+Language LanguageConverter::convert(const QString & language) {
+	static QMap<QString, Language> languageStrings = {
+		{ QStringLiteral("Deutsch"), Language::German },
+		{ QStringLiteral("English"), Language::English },
+		{ QStringLiteral("Polish"), Language::Polish },
+		{ QStringLiteral("Russian"), Language::Russian },
+	};
+
+	return languageStrings.value(language, None);
+}
+
