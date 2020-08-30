e6bb22339db1e272e86eb7dcbf909c3d4d4db68b
Daniel Bonrath <bonne@clockwork-origins.de>
2020-08-30 10:42:37 +0200

    SP-1010 language column in database not editable anymore

--- src/client/widgets/ModDatabaseView.cpp
+++ src/client/widgets/ModDatabaseView.cpp
@@ -779,6 +779,7 @@ void ModDatabaseView::updateModList(std::vector<common::Mod> mods) {
 		idItem->setEditable(false);
 
 		QStandardItem * languagesItem = new QStandardItem();
+		languagesItem->setEditable(false);
 
 		const auto pm = languagePixmaps[mod.supportedLanguages];
 		
