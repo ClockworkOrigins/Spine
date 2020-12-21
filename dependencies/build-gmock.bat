6fcef8a789bec8c4997b223f79f023fe1ac52fba
Daniel Bonrath <bonne@clockwork-origins.de>
2020-12-21 16:07:28 +0100

    SP-1068 added debug lib of gtest

--- dependencies/build-gmock.bat
+++ dependencies/build-gmock.bat
@@ -1,34 +1,52 @@
 @echo OFF
 
+IF "%1" == "build" (goto build)
+
 call build-common.bat %1 %2
+
+echo "Extracting GoogleMock with GoogleTest"
 
 Set ARCHIVE=release-1.10.0.zip
 Set BUILD_DIR=%TMP_DIR%/googletest-release-1.10.0
-Set PREFIX=%DEP_DIR%/%ARCH_DIR%/gmock
+
+call build-common.bat downloadAndUnpack %ARCHIVE% %BUILD_DIR% https://github.com/google/googletest/archive/
+
+cd %DEP_DIR%
+
+call build-gmock.bat build Debug
+call build-gmock.bat build Release
+
+cd %DEP_DIR%
+RD /S /Q "%TMP_DIR%"
+
+EXIT /B
+
+:build
+
+Set PREFIX=%DEP_DIR%/%ARCH_DIR%/gmock/%2
 
 IF EXIST %PREFIX% EXIT /B
 
 echo "Compile GoogleMock with GoogleTest"
 
-echo "Extracting GoogleMock with GoogleTest"
-
-call build-common.bat downloadAndUnpack %ARCHIVE% %BUILD_DIR% https://github.com/google/googletest/archive/
-
 echo "Configuring GoogleMock with GoogleTest"
 
-cd %BUILD_DIR%
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_SHARED_LIBS=ON -G "%VSCOMPILER%" -A "%VSARCH%" .
+IF EXIST "%BUILD_DIR%\spineBuild" RD /S / Q "%BUILD_DIR%\spineBuild"
+
+mkdir "%BUILD_DIR%\spineBuild"
+
+pushd "%BUILD_DIR%\spineBuild"
+
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_SHARED_LIBS=ON -G "%VSCOMPILER%" -A "%VSARCH%" ..
 
 echo "Building GoogleMock with GoogleTest"
 
-cmake --build . --config Release
+cmake --build . --config %2
 
 echo "Installing GoogleMock with GoogleTest"
 
-cmake --build . --config Release --target install
+cmake --build . --config %2 --target install
 
 echo "Cleaning up"
 
-cd %DEP_DIR%
-RD /S /Q "%TMP_DIR%"
-
+popd
