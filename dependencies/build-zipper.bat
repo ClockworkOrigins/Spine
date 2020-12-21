774c400e92772836126c5d0145c1b0fcfcf80658
Daniel Bonrath <bonne@clockwork-origins.de>
2020-12-21 17:21:24 +0100

    SP-1068 added debug library for zlib and zipper

--- dependencies/build-zipper.bat
+++ dependencies/build-zipper.bat
@@ -1,15 +1,12 @@
 @echo OFF
 
+IF "%1" == "build" (goto build)
+
 call build-common.bat %1 %2
 
+echo "Extracting zipper"
+
 Set BUILD_DIR=%TMP_DIR%/zipper
-Set PREFIX=%DEP_DIR%/%ARCH_DIR%/zipper
-
-IF EXIST %PREFIX% EXIT /B
-
-echo "Compile zipper"
-
-echo "Cloning zipper"
 
 IF NOT EXIST %TMP_DIR% MKDIR %TMP_DIR%
 
@@ -19,20 +16,42 @@ git clone --branch v1.0.1 --recursive https://github.com/sebastiandev/zipper.git
 
 popd
 
-echo "Configuring zipper"
+cd %DEP_DIR%
 
-cd %BUILD_DIR%
-cmake -DBUILD_SHARED_VERSION=OFF -DBUILD_STATIC_VERSION=ON -DBUILD_TEST=OFF -DLIBZ_INCLUDE_DIR=%DEP_DIR%/%ARCH_DIR%/zlib/include -DLIBZ_LIBRARY=%DEP_DIR%/%ARCH_DIR%/zlib/lib/zlibstatic.lib -DCMAKE_INSTALL_PREFIX=%PREFIX% -G "%VSCOMPILER%" -A "%VSARCH%" .
-
-echo "Building zipper"
-
-cmake --build . --config Release
-
-echo "Installing zipper"
-
-cmake --build . --config Release --target install
-
-echo "Cleaning up"
+call build-zipper.bat build Debug d
+call build-zipper.bat build Release
 
 cd %DEP_DIR%
 RD /S /Q "%TMP_DIR%"
+
+EXIT /B
+
+:build
+
+Set PREFIX=%DEP_DIR%/%ARCH_DIR%/zipper/%2
+
+IF EXIST %PREFIX% EXIT /B
+
+echo "Compile zipper"
+
+echo "Configuring zipper"
+
+IF EXIST "%BUILD_DIR%\spineBuild" RD /S / Q "%BUILD_DIR%\spineBuild"
+
+mkdir "%BUILD_DIR%\spineBuild"
+
+pushd "%BUILD_DIR%\spineBuild"
+
+cmake -DBUILD_SHARED_VERSION=OFF -DBUILD_STATIC_VERSION=ON -DBUILD_TEST=OFF -DLIBZ_INCLUDE_DIR=%DEP_DIR%/%ARCH_DIR%/zlib/%2/include -DLIBZ_LIBRARY=%DEP_DIR%/%ARCH_DIR%/zlib/lib/%2/zlibstatic%3.lib -DCMAKE_INSTALL_PREFIX=%PREFIX% -G "%VSCOMPILER%" -A "%VSARCH%" ..
+
+echo "Building zipper"
+
+cmake --build . --config %2
+
+echo "Installing zipper"
+
+cmake --build . --config %2 --target install
+
+echo "Cleaning up"
+
+popd
--- dependencies/build-zlib.bat
+++ dependencies/build-zlib.bat
@@ -1,34 +1,53 @@
 @echo OFF
 
+IF "%1" == "build" (goto build)
+
 call build-common.bat %1 %2
+
+echo "Extracting zlib"
 
 Set ARCHIVE=zlib1211.zip
 Set BUILD_DIR=%TMP_DIR%/zlib-1.2.11
-Set PREFIX=%DEP_DIR%/%ARCH_DIR%/zlib
+
+call build-common.bat downloadAndUnpack %ARCHIVE% %BUILD_DIR% https://zlib.net/
+
+cd %DEP_DIR%
+
+call build-zlib.bat build Debug
+call build-zlib.bat build Release
+
+cd %DEP_DIR%
+RD /S /Q "%TMP_DIR%"
+
+EXIT /B
+
+:build
+
+Set PREFIX=%DEP_DIR%/%ARCH_DIR%/zlib/%2
 
 IF EXIST %PREFIX% EXIT /B
 
 echo "Compile zlib"
 
-echo "Extracting zlib"
-
-call build-common.bat downloadAndUnpack %ARCHIVE% %BUILD_DIR% https://zlib.net/
-
 echo "Configuring zlib"
 
-cd %BUILD_DIR%
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -G "%VSCOMPILER%" -A "%VSARCH%" .
+IF EXIST "%BUILD_DIR%\spineBuild" RD /S / Q "%BUILD_DIR%\spineBuild"
+
+mkdir "%BUILD_DIR%\spineBuild"
+
+pushd "%BUILD_DIR%\spineBuild"
+
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -G "%VSCOMPILER%" -A "%VSARCH%" ..
 
 echo "Building zlib"
 
-MSBuild.exe zlib.sln /m:%NUMBER_OF_PROCESSORS% /p:Configuration=Release
+cmake --build . --config %2
 
 echo "Installing zlib"
 
-MSBuild.exe INSTALL.vcxproj /p:Configuration=Release
+cmake --build . --config %2 --target install
 
 echo "Cleaning up"
 
-cd %DEP_DIR%
-RD /S /Q "%TMP_DIR%"
+popd
 
